04a5212724025d83854872dd3b1692edbc830a04
abhijeetatmindstix <113978688+abhijeetatmindstix@users.noreply.github.com>
2022-11-01 14:50:20 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,3 +1,13 @@
 def call(name){
   echo "Hey ${name}, How are you?"
 }
+
+def add(x,y)
+{
+  echo "sum of ${x} and ${y} is ${x+y}"
+}
+
+def mul(x,y)
+{
+  echo ",multiple of ${x} and ${y} is ${x*y}"
+}
